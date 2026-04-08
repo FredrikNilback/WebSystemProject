25a8defc0751765500bd8b29f1ade94de9b28735
Fredrik <fredrik.nilback@gmail.com>
2026-04-08 22:32:37 +0200

    add manage-users.php

--- app/db.php
+++ app/db.php
@@ -1,10 +1,43 @@
 <?php
-$secrets = json_decode(file_get_contents('../../../secrets.json'));
 
-$host = "localhost";
-$username = $secrets->db_usr;
-$dbName = $secrets->db_name;
-$password = $secrets->db_pwd;
+    function getDataBase() {
+        $secretsPath = __DIR__ . '/../secrets.json';
+        $secrets = json_decode(file_get_contents($secretsPath));
 
-$mysqli = new mysqli($host, $username, $password, $dbName);
+        $host = "localhost";
+        $username = $secrets->db_usr;
+        $dbName = $secrets->db_name;
+        $password = $secrets->db_pwd;
+
+        $mysqli = new mysqli($host, $username, $password, $dbName);
+
+        if ($mysqli->connect_error) {
+            die("Could not establish database connection");
+        }
+
+        return $mysqli;
+    }
+
+    function createUser($username, $firstname, $lastname, $email, $password, $role) {
+        $mysqli = getDataBase();
+        $passwordHash =  password_hash($password, PASSWORD_DEFAULT);
+
+        $stmt = $mysqli->prepare("INSERT INTO user (username, first_name, last_name, user_email, password_hash, user_role)
+                                  VALUES (?, ?, ?, ?, ?, ?)");
+
+        $stmt->bind_param(
+            "ssssss",
+            $username,
+            $firstname,
+            $lastname,
+            $email,
+            $passwordHash,
+            $role
+        );
+
+        $stmt->execute();
+        $stmt->close();
+        $mysqli->close();
+    }
+
 ?>
--- public_html/Project/manage-users.php
+++ public_html/Project/manage-users.php
@@ -1,9 +1,33 @@
 <?php
     $activePage = 'manage-users';
+    require_once '../../app/db.php';
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $username  = $_POST['username'] ?? '';
+        $firstname = $_POST['firstname'] ?? '';
+        $lastname  = $_POST['lastname'] ?? '';
+        $email     = $_POST['email'] ?? '';
+        $password  = $_POST['password'] ?? '';
+        $role      = $_POST['role'] ?? '';
+
+        //createUser($username, $firstname, $lastname, $email, $password, $role);
+    }
 ?>
 
 <?php require_once 'includes/header.php' ?>
-    <div class='content'>
-
+    <div class="content">
+        <form method="POST">
+            <input type="text" name="username" placeholder="Username" required />
+            <input type="text" name="firstname" placeholder="First Name" required />
+            <input type="text" name="lastname" placeholder="Last Name" required />
+            <input type="email" name="email" placeholder="Email" required />
+            <input type="password" name="password" placeholder="Password" required />
+            <select name="role" required>
+                <option value="">Select role</option>
+                <option value="reporter">Reporter</option>
+                <option value="responder">Responder</option>
+                <option value="administrator">Administrator</option>
+            </select>
+            <button type="submit">Create User</button>
+        </form>
     </div>
 <?php require_once 'includes/footer.php' ?>
